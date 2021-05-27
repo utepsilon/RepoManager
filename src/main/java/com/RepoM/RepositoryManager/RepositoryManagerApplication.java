af92d35e5ba3bb19a71c935ff8ebe09f3db44103
Utkarsh Singh <vibhu029@gmai.com>
2021-05-27 23:50:09 +0530

    initialcommit

---
+++ src/main/java/com/RepoM/RepositoryManager/RepositoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.RepoM.RepositoryManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RepoM/RepositoryManager/RepositoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RepoM.RepositoryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
